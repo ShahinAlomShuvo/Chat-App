0094c11b866ab566e559d67ac73e766a3f439d3a
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2024-09-06 19:24:42 +0600

    create user interface & model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,43 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "faculty", "student"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+      default: "in-progress",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model<TUser>("User", userSchema);
